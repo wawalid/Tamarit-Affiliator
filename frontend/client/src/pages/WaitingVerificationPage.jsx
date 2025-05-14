48ce8ae0da3ea25770883b6e33cddc6d202027e4
wawalid <walidtoubi89@gmail.com>
2025-05-14 23:12:36 +0200

    Update WaitingVerificationPage.jsx
    arreglado el bug de que cuando se cambiada el estado de false a true se mantenia el usuario en la pagina de waiting

--- frontend/client/src/pages/WaitingVerificationPage.jsx
+++ frontend/client/src/pages/WaitingVerificationPage.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function WaitingVerificationPage() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user?.is_verified) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-900 text-white">
       <div className="bg-zinc-800 p-8 rounded-2xl shadow-xl text-center w-full max-w-md">
