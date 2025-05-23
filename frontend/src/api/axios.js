3f37d14b4c4d1c8b6a0e32458e8a09fea25a690f
wawalid <walidtoubi89@gmail.com>
2025-05-23 13:19:21 +0200

    probando a poner withcredentials en false

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 
 const instance = axios.create({
     baseURL: `${BACKEND_URL}/api`,
-    withCredentials: true,
+    withCredentials: false,
 })
 
 export default instance;
