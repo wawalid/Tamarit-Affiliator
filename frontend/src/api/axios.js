e4355aeac0104e542597f6cdf54da6e0bd4bdd6c
wawalid <walidtoubi89@gmail.com>
2025-05-23 10:59:09 +0200

    preparando para desplegar

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000/api';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: BACKEND_URL,
     withCredentials: true,
 })
 
