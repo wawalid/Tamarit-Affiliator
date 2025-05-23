328b32ca11007f8b1922a923b734ba7c08767aa1
wawalid <walidtoubi89@gmail.com>
2025-05-23 13:24:32 +0200

    cookie estricta

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -7,11 +7,14 @@ import { TOKEN_SECRET } from "../config.js";
 
 
 const cookieOptions = {
-  httpOnly: true,         
-  secure: true,           
-  sameSite: "none",       
-  path: "/",              
+  httpOnly: true,
+  secure: true,
+  sameSite: "strict",
+  priority: "high", // Esta opción puede no ser reconocida por todos los navegadores aún
+  path: "/",
+  expires: new Date(Date.now() + 15 * 60 * 1000), // 15 minutos
 };
+
 
 export const verifyToken = async (req, res) => {
   const { token } = req.cookies;
