a8cc594a59987844b83d3e261cc74eefa3a0e7e1
wawalid <walidtoubi89@gmail.com>
2025-05-23 13:22:01 +0200

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 
 const instance = axios.create({
     baseURL: `${BACKEND_URL}/api`,
-    withCredentials: false,
+    withCredentials: true,
 })
 
 export default instance;
